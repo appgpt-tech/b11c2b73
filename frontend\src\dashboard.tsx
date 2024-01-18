bea3d4ed9a2334cf4c307e9031c18c857c5a1167
11ai <124818466+11ai@users.noreply.github.com>
2024-01-18 20:41:46 +0200

    created on Thu Jan 18 2024 20:41:34 GMT+0200 (Eastern European Standard Time)

---
+++ frontend\src\dashboard.tsx
@@ -0,0 +1,7 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title= "picatchu" />
+      <CardContent> A web app to track my collection of Pokemon collectible cards. < /CardContent>
+      </Card>
+    );

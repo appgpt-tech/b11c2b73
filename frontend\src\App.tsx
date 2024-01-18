9b75a8de124a2c5c1ed01e84f59458870ddc1724
11ai <124818466+11ai@users.noreply.github.com>
2024-01-18 20:41:39 +0200

    created on Thu Jan 18 2024 20:41:34 GMT+0200 (Eastern European Standard Time)

---
+++ frontend\src\App.tsx
@@ -0,0 +1,98 @@
+
+// in src/App.tsx
+import { Admin, Resource, CustomRoutes } from "react-admin";
+import postgrestRestProvider from "@promitheus/ra-data-postgrest";
+import fakeDataProvider from "ra-data-fakerest";
+import { Dashboard } from "./dashboard";
+import { authProvider, apInitialize } from "./authProvider";
+import { i18nProvider } from "./i18nProvider";
+import LoginPage, { Login } from "./Login";
+import data from "./data";
+import { UserList, UserCreate, UserEdit} from "./resources/User";
+import { CardList, CardCreate, CardEdit} from "./resources/Card";
+import UserIcon from "@mui/icons-material/Person";
+import CardIcon from "@mui/icons-material/CreditCard"; 
+// SUPERTOKENS
+import React from "react";
+import SuperTokens, {
+  SuperTokensWrapper,
+  getSuperTokensRoutesForReactRouterDom,
+} from "supertokens-auth-react";
+import ThirdPartyPasswordless from "supertokens-auth-react/recipe/thirdpartypasswordless";
+import Session from "supertokens-auth-react/recipe/session";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import * as reactRouterDom from "react-router-dom";
+let sessionFn = Session.init();
+SuperTokens.init({
+  appInfo: {
+    appName: import.meta.env.VITE_SUPERTOKENS_APPNAME,
+    apiDomain: import.meta.env.VITE_BACKEND_DOMAIN,
+    websiteDomain: import.meta.env.VITE_SUPERTOKENS_WEBSITEDOMAIN,
+    apiBasePath: import.meta.env.VITE_BACKEND_APIPATH + "/auth",
+    websiteBasePath: import.meta.env.VITE_SUPERTOKENS_WEBSITEBASEPATH,
+  },
+  recipeList: [
+    ThirdPartyPasswordless.init({
+      contactMethod: "EMAIL",
+      signInUpFeature: {
+        providers: [
+          ThirdPartyPasswordless.Github.init(),
+          //ThirdPartyPasswordless.Google.init(),
+          //ThirdPartyPasswordless.Facebook.init(),
+          //ThirdPartyPasswordless.Apple.init(),
+        ],
+      },
+    }),
+    sessionFn,
+  ],
+});
+apInitialize(Session);
+// END SUPERTOKENS
+let dataProvider: any;
+if (import.meta.env.VITE_USE_BACKEND_DATA === "true") {
+  dataProvider = postgrestRestProvider(
+    import.meta.env.VITE_BACKEND_DOMAIN +
+      import.meta.env.VITE_BACKEND_APIPATH +
+      "/proxy"
+  );
+} else {
+  dataProvider = fakeDataProvider(data.defaultData);
+}
+
+const App = () => (
+  <SuperTokensWrapper>
+    <BrowserRouter basename="/b11c2b73">
+      <Admin
+        authProvider={
+          import.meta.env.VITE_ENVIRONMENT != "DEV" ? authProvider : undefined
+        }
+        requireAuth
+        loginPage={LoginPage}
+        dataProvider={dataProvider}
+        i18nProvider={i18nProvider}
+        dashboard={Dashboard}
+        
+      >
+    <Resource name="User" options={{label:"user"}} 
+list={UserList}
+create={UserCreate}
+edit={UserEdit}
+recordRepresentation="userid"
+icon={UserIcon}/>
+<Resource name="Card" options={{label:"card"}} 
+list={CardList}
+create={CardCreate}
+edit={CardEdit}
+recordRepresentation="cardid"
+icon={CardIcon}/>
+    <CustomRoutes noLayout>
+      {/*This renders the login UI on the /auth route*/}
+      {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+      {/*Your app routes*/}
+    </CustomRoutes>
+  </Admin>
+  </BrowserRouter>
+  </SuperTokensWrapper>
+);
+
+export default App;

d7c388103e77a7d6da33f66ad3487d4212b4c3a2
11ai <124818466+11ai@users.noreply.github.com>
2024-01-23 05:31:01 +0200

    created on Tue Jan 23 2024 03:30:42 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/dashboard.tsx
@@ -0,0 +1,7 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+    export const Dashboard = () => (
+      <Card>
+      <CardHeader title= "studytracker" />
+      <CardContent> An application to help parents track their children's attendance in various after school lessons. < /CardContent>
+      </Card>
+    );

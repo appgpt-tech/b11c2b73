9c7f3c50fba383514ae2a48d676b4b27d2bf0dc1
11ai <124818466+11ai@users.noreply.github.com>
2024-01-23 05:30:49 +0200

    created on Tue Jan 23 2024 03:30:41 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,106 @@
+
+// in src/App.tsx
+import { Admin, Resource, CustomRoutes } from "react-admin";
+import postgrestRestProvider from "@promitheus/ra-data-postgrest";
+import fakeDataProvider from "ra-data-fakerest";
+import { Dashboard } from "./dashboard";
+import { authProvider, apInitialize } from "./authProvider";
+import { i18nProvider } from "./i18nProvider";
+import LoginPage, { Login } from "./Login";
+import data from "./data";
+import { ChildrenList, ChildrenCreate, ChildrenEdit} from "./resources/Children";
+import { LessonsList, LessonsCreate, LessonsEdit} from "./resources/Lessons";
+import { AttendanceList, AttendanceCreate, AttendanceEdit} from "./resources/Attendance";
+import ChildrenIcon from "@mui/icons-material/ChildCare";
+import LessonsIcon from "@mui/icons-material/Book";
+import AttendanceIcon from "@mui/icons-material/Event"; 
+// SUPERTOKENS
+import React from "react";
+import SuperTokens, {
+  SuperTokensWrapper,
+  getSuperTokensRoutesForReactRouterDom,
+} from "supertokens-auth-react";
+import ThirdPartyPasswordless from "supertokens-auth-react/recipe/thirdpartypasswordless";
+import Session from "supertokens-auth-react/recipe/session";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import * as reactRouterDom from "react-router-dom";
+let sessionFn = Session.init();
+SuperTokens.init({
+  appInfo: {
+    appName: import.meta.env.VITE_SUPERTOKENS_APPNAME,
+    apiDomain: import.meta.env.VITE_BACKEND_DOMAIN,
+    websiteDomain: import.meta.env.VITE_SUPERTOKENS_WEBSITEDOMAIN,
+    apiBasePath: import.meta.env.VITE_BACKEND_APIPATH + "/auth",
+    websiteBasePath: import.meta.env.VITE_SUPERTOKENS_WEBSITEBASEPATH,
+  },
+  recipeList: [
+    ThirdPartyPasswordless.init({
+      contactMethod: "EMAIL",
+      signInUpFeature: {
+        providers: [
+          ThirdPartyPasswordless.Github.init(),
+          //ThirdPartyPasswordless.Google.init(),
+          //ThirdPartyPasswordless.Facebook.init(),
+          //ThirdPartyPasswordless.Apple.init(),
+        ],
+      },
+    }),
+    sessionFn,
+  ],
+});
+apInitialize(Session);
+// END SUPERTOKENS
+let dataProvider: any;
+if (import.meta.env.VITE_USE_BACKEND_DATA === "true") {
+  dataProvider = postgrestRestProvider(
+    import.meta.env.VITE_BACKEND_DOMAIN +
+      import.meta.env.VITE_BACKEND_APIPATH +
+      "/proxy"
+  );
+} else {
+  dataProvider = fakeDataProvider(data.defaultData);
+}
+
+const App = () => (
+  <SuperTokensWrapper>
+    <BrowserRouter basename="/b11c2b73">
+      <Admin
+        authProvider={
+          import.meta.env.VITE_ENVIRONMENT != "DEV" ? authProvider : undefined
+        }
+        requireAuth
+        loginPage={LoginPage}
+        dataProvider={dataProvider}
+        i18nProvider={i18nProvider}
+        dashboard={Dashboard}
+        
+      >
+    <Resource name="Children" options={{label:"Children"}} 
+list={ChildrenList}
+create={ChildrenCreate}
+edit={ChildrenEdit}
+recordRepresentation="childid"
+icon={ChildrenIcon}/>
+<Resource name="Lessons" options={{label:"Lessons"}} 
+list={LessonsList}
+create={LessonsCreate}
+edit={LessonsEdit}
+recordRepresentation="lessonid"
+icon={LessonsIcon}/>
+<Resource name="Attendance" options={{label:"Attendance"}} 
+list={AttendanceList}
+create={AttendanceCreate}
+edit={AttendanceEdit}
+recordRepresentation="id"
+icon={AttendanceIcon}/>
+    <CustomRoutes noLayout>
+      {/*This renders the login UI on the /auth route*/}
+      {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+      {/*Your app routes*/}
+    </CustomRoutes>
+  </Admin>
+  </BrowserRouter>
+  </SuperTokensWrapper>
+);
+
+export default App;

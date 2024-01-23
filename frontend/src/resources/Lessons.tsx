dfc9b413d6c87b1a91f63d34cd1aa69905dc51e0
11ai <124818466+11ai@users.noreply.github.com>
2024-01-23 05:30:58 +0200

    created on Tue Jan 23 2024 03:30:42 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/Lessons.tsx
@@ -0,0 +1,89 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const LessonsTitle = () => {
+  const record = useRecordContext();
+  return <span>Lessons {record ? `"${ record.Lessonid }"` : ""}</span>;
+};
+
+export const LessonsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="Lessonid" />
+<TextField source="Topic" />
+<TextField source="Costperlesson" />
+<NumberField source="Id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const LessonsEdit = () => (
+                    <Edit title={<LessonsTitle />}>
+                      <SimpleForm>
+                          <TextInput source="Lessonid"   />
+<TextInput source="Topic"   />
+<TextInput source="Costperlesson"   />
+<NumberInput source="Id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const LessonsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="Lessonid"   />
+<TextInput source="Topic"   />
+<TextInput source="Costperlesson"   />
+<NumberInput source="Id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+
+    ];
+
+

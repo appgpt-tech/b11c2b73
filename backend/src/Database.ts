6ce032a74e5291ecb3122be591797e41e8d926f4
11ai <124818466+11ai@users.noreply.github.com>
2024-01-23 05:39:13 +0200

    edited on Tue Jan 23 2024 03:38:49 GMT+0000 (Coordinated Universal Time)

--- backend/src/Database.ts
+++ backend/src/Database.ts
@@ -3,8 +3,8 @@ import { DataSource } from "typeorm";
 import { DBConfiguration } from "./Configuration";
 import { SettingsEntity } from "./db/Settings.entity";
 //autogenerate imports based on resources
-import { ChildrenEntity } from "./db/Children.entity";
-import { LessonsEntity } from "./db/Lessons.entity";
+import { StudentEntity } from "./db/Student.entity";
+import { ActivityEntity } from "./db/Activity.entity";
 import { AttendanceEntity } from "./db/Attendance.entity";
 
 export class Database {
@@ -16,7 +16,7 @@ export class Database {
     let dbConfig: any = dbConfiguration as any;
     //Autogenerate entities array from resource names
 
-    dbConfig.entities = [SettingsEntity, ChildrenEntity, LessonsEntity, AttendanceEntity];
+    dbConfig.entities = [SettingsEntity, StudentEntity, ActivityEntity, AttendanceEntity];
     Database.ds = new DataSource(dbConfig);
     await Database.ds.initialize();
 
@@ -26,15 +26,15 @@ export class Database {
     await Database.Seed();
   }
   static async Seed() {
-    let data: any = {"Children":[],"Lessons":[],"Attendance":[]};
+    let data: any = {"Student":[],"Activity":[],"Attendance":[]};
     //Autogenerate multiple such calls ie for each resource and its data object
     let isSeeded = await this.IsSeeded();
     //if (!isSeeded) {
     //forcing app recreation
     if (true){
       console.log('   Seeding database...');
-      await this.SeedResource("ChildrenEntity", data.Children);
-await this.SeedResource("LessonsEntity", data.Lessons);
+      await this.SeedResource("StudentEntity", data.Student);
+await this.SeedResource("ActivityEntity", data.Activity);
 await this.SeedResource("AttendanceEntity", data.Attendance); 
       await this.SeedResource("SettingsEntity", {
         settingname: "isSeeded",

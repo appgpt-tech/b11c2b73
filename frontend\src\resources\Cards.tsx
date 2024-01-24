ef1a43eac522bc033ebc21683bd813b785826c2e
11ai <124818466+11ai@users.noreply.github.com>
2024-01-24 21:45:29 +0200

    created on Wed Jan 24 2024 21:45:13 GMT+0200 (Eastern European Standard Time)

---
+++ frontend\src\resources\Cards.tsx
@@ -0,0 +1,93 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const CardsTitle = () => {
+  const record = useRecordContext();
+  return <span>Cards {record ? `"${ record.Cardid }"` : ""}</span>;
+};
+
+export const CardsList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="Cardid" />
+<TextField source="Cardname" />
+<TextField source="Serial" />
+<TextField source="Price" />
+<NumberField source="Id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const CardsEdit = () => (
+                    <Edit title={<CardsTitle />}>
+                      <SimpleForm>
+                          <TextInput source="Cardid"   />
+<TextInput source="Cardname"   />
+<TextInput source="Serial"   />
+<TextInput source="Price"   />
+<NumberInput source="Id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const CardsCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="Cardid"   />
+<TextInput source="Cardname"   />
+<TextInput source="Serial"   />
+<TextInput source="Price"   />
+<NumberInput source="Id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+,
+
+    ];
+
+

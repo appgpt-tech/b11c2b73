c5d414f1bad92f324159bc704aa9d8eeab20724d
11ai <124818466+11ai@users.noreply.github.com>
2024-01-24 21:45:23 +0200

    edited on Wed Jan 24 2024 21:45:13 GMT+0200 (Eastern European Standard Time)

--- frontend\src\i18nProvider.ts
+++ frontend\src\i18nProvider.ts
@@ -3,7 +3,7 @@
     import polyglotI18nProvider from "ra-i18n-polyglot";
     import enOriginal from 'ra-language-english';
 
-    const enResources = { resources: {"User":{"name":"user","fields":{"Userid":"Userid","Email":"Email","Name":"Name","Id":"Id"}},"Card":{"name":"card","fields":{"Cardid":"Cardid","Cardname":"Cardname","Serial":"Serial","Price":"Price","Serialxref":"Serialxref","Id":"Id"}}}};
+    const enResources = { resources: {"Users":{"name":"Users","fields":{"Userid":"Userid","Email":"Email","Name":"Name","Id":"Id"}},"Cards":{"name":"Cards","fields":{"Cardid":"Cardid","Cardname":"Cardname","Serial":"Serial","Price":"Price","Id":"Id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);

3d70520898d90cfdeba922a4507564e960d15fde
11ai <124818466+11ai@users.noreply.github.com>
2024-01-24 21:58:23 +0200

    edited on Wed Jan 24 2024 19:58:13 GMT+0000 (Coordinated Universal Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -2,6 +2,6 @@ import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
       <CardHeader title= "lesson tracker" />
-      <CardContent> An app to help parents track their children's after school lessons/activities attendance < /CardContent>
+      <CardContent> An app to help parents track their children's after-school lessons/activities and attendance < /CardContent>
       </Card>
     );

8e44e518fafbaa48e4d65ac54b31c04881fcaf8f
11ai <124818466+11ai@users.noreply.github.com>
2024-01-24 21:45:24 +0200

    edited on Wed Jan 24 2024 21:45:12 GMT+0200 (Eastern European Standard Time)

--- frontend\src\dashboard.tsx
+++ frontend\src\dashboard.tsx
@@ -2,6 +2,6 @@ import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
       <CardHeader title= "picatchu" />
-      <CardContent> A web app to track my collection of Pokemon collectible cards. < /CardContent>
+      <CardContent> A web app to track my collection of Pokemon collectible cards < /CardContent>
       </Card>
     );
